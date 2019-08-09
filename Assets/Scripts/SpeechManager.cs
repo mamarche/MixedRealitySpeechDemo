993559cc317b12e593b40db8717b440172268133
Marcello Marchetti <mamarche@outlook.it>
2019-08-09 16:45:39 +0200

    Project Scaffolding

---
+++ Assets/Scripts/SpeechManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechManager : MonoBehaviour
+{
+    public MeshRenderer cubeRenderer;
+    public Material redMaterial;
+    public Material blueMaterial;
+
+    public void ColorRedCommand()
+    {
+        cubeRenderer.material = redMaterial;
+    }
+
+    public void ColorBlueCommand()
+    {
+        cubeRenderer.material = blueMaterial;
+    }
+}
